05e9a3c5d4e120d46667f44c439b063b6e38ccde
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2024-04-29 13:02:01 +0200

    :star2:  Add detect_date_format fucntion
    This commit adds new `detect_date_format` function to address issue #7

---
+++ R/detect_date_format.R
@@ -0,0 +1,55 @@
+
+#' Detect the Date Format of a Vector of Dates
+#'
+#' This function automatically detects and returns the most likely date format
+#' from a given vector of date strings. It attempts to parse the dates using
+#' a set of common date formats and selects the format that successfully parses
+#' the majority of the entries.
+#'
+#' @param date_vector A character vector of dates.
+#' @param common_formats An optional character vector specifying the date 
+#'        formats to try. If NULL, a default set of formats is used.
+#' @param ... Additional arguments passed to \code{lubridate::parse_date_time}.
+#'
+#' @return A character string representing the date format that had the highest
+#'         success rate of parsing the dates in \code{date_vector}. Returns
+#'         \code{NA} if no suitable format is found.
+#'
+#' @examples
+#' # date_vector <- c("01-02-2021", "03-15-1988", "2023-05-12",
+#' #                  "19/02/2021", "1988-15-03", "05-12-2023", "05.27.2022")
+#' # detect_date_format(date_vector)
+#'
+#' @export
+detect_date_format <- function(date_vector, common_formats = NULL, ...) {
+  
+  # Provide default common formats if not specified ----------------------------
+  if (is.null(common_formats)) {
+    common_formats <- c("%d-%m-%Y", "%m-%d-%Y", "%Y-%m-%d",
+                        "%d/%m/%Y", "%m/%d/%Y", "%Y/%m/%d",
+                        "%d.%m.%Y", "%m.%d.%Y", "%Y.%m.%d", 
+                        "%Y %m %d",  "%Y%m%d",
+                        "%Y-%m-%d %H:%M:%S",
+                        "%d/%m/%Y %I:%M %p",
+                        "%m.%d.%Y %H-%M-%S",
+                        "%Y %b %d %H%M",
+                        "%d-%m-%Y %H:%M",
+                        "%m/%d/%Y %I:%M %p"
+    )
+  }
+  
+  # Calculate the number of successful parses for each format ------------------
+  format_counts <- sapply(common_formats, function(fmt) {
+    sum(!is.na(
+      lubridate::parse_date_time(
+        date_vector, orders = fmt, quiet = TRUE, exact = TRUE,
+        locale = Sys.setlocale("LC_TIME", "C"), ...)))
+  }, USE.NAMES = TRUE)
+  
+  # Select the format with the maximum count of successful parses --------------
+  if (max(format_counts) > 0) {
+    return(names(which.max(format_counts)))
+  } else {
+    return(NA)  
+  }
+}
---
+++ tests/testthat/test-detect_date_format.R
@@ -0,0 +1,22 @@
+testthat::test_that("detects simple date formats correctly", {
+  testthat::expect_equal(
+    detect_date_format(c("01-02-2021", "02-03-2021")), "%d-%m-%Y")
+  testthat::expect_equal(
+    detect_date_format(c("2021/02/01", "2021/03/02")), "%Y/%m/%d")
+})
+
+testthat::test_that("handles mixed format vectors", {
+  dates <- c("01-02-2021", "2021/03/02", "04.05.2021")
+  testthat::expect_equal(detect_date_format(dates), "%d-%m-%Y")
+})
+
+testthat::test_that("returns NA when no dates are parsed", {
+  dates <- c("99-99-9999", "99/99/9999")
+  testthat::expect_true(is.na(detect_date_format(dates)))
+})
+
+testthat::test_that("handles additional arguments correctly", {
+  dates <- c("01-02-21", "02-03-21")  # Ambiguous year format
+  testthat::expect_equal(detect_date_format(
+    dates, common_formats = c("%d-%m-%y")), "%d-%m-%y")
+})
